a10a5faa51cf23d3af3dcfda0343e862283195f7
Jessica Armoa <85409054+jessica-armoa@users.noreply.github.com>
2023-08-24 01:43:48 -0400

    Ejercicio core Contador con destroy

--- Core/Contador/server.py
+++ Core/Contador/server.py
@@ -2,17 +2,18 @@ from flask import Flask, render_template, redirect, session
 app = Flask(__name__)
 app.secret_key = 'clave ultra secreta'
 
-
-
 @app.route('/')
 def contar():
-  session['contador'] = 0
-  return redirect('/contador')
-
-@app.route('/contador')
-def mostrar():
-  session['contador'] += 1
+  if 'contador' in session:
+    session['contador'] += 1
+  else:
+    session['contador'] = 1
   return render_template('contador.html')
+
+@app.route('/destroy_session')
+def destruir():
+  session.pop('contador')
+  return redirect('/')
 
 if __name__=="__main__":
     app.run(debug=True)
